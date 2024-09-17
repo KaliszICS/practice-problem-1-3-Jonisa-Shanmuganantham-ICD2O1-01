234e5cc61c608660521cf332be9a0350bd918526
Jonisa-Shanmuganantham-ICD2O1-01 <jonisa.shanmuganantham@student.tdsb.on.ca>
2024-09-17 14:06:56 +0000

    first commit

--- PP1_3.py
+++ PP1_3.py
@@ -1,18 +1,24 @@
-
+'''
+    Lesson: Newline character, print without newline, escape character
+    Author: Jonisa Shanmuganantham
+    Date Created: Sept 17, 2024
+    Date Last Modified: Sept 17, 2024
+'''
 
 def q1():
-  #Write Assignment code here
-  #Remember your code must be indented once
+  print(" * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n")
+  print("* * * * *\n*       *\n*       *\n*       *\n* * * * *\n")
+  print("* * * * * *\n *       *\n  *     *\n   *   *\n    * *\n     *\n")
 
 def q2():
-  #Write Assignment code here
-  
-def q3():
-  #Write Assignment code here
-  
+    print("Hello ", end = "")
+    print("World\n")
 
+def q3():
+  print('"Man"\n"Board"\n"Man Overboard"')
 
 #Do not alter anything after this line
 q1()
 q2()
 q3()
+
